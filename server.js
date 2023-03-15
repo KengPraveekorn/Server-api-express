3168bf1d5bd925e609dfddda25f14ff4e925c68c
Praveekorn Chianglong <Praveekorn@gmail.com>
2023-03-15 23:33:23 +0700

    update11.33

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 // 3 ประสาน Middleware --> ทำฉันด้วย ทำฉันก่อน ทำฉันตลอด
 const morgan = require('morgan')
 const bodyParser = require('body-parser')  //  รับส่งข้อมูล JSON จากหน้าบ้านมาหลังบ้าน
-const cors = require('cors')
+const cors = require('cors')  // 
 
 const {readdirSync} = require('fs')  // เรียกใช้ func สำหรับอ่าน folder auto 
 const connectDB = require('./Config/db')
@@ -23,6 +23,7 @@ connectDB()
 // middleware
 app.use(morgan("dev"))
 app.use(bodyParser.json({ limit: "10mb" }))
+app.use(cors())
 
 
 // วิธี Route 1
